ebe023a9db021331f6ec72d83abb93445bc4ecdd
Eugenia Kim <elk303@nyu.edu>
2019-07-29 21:19:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
-# def nyc_pigeon_organizer(data)
-#   pigeon_list = {}
-#   data.each do |keys, value|
-#     value.each do |trait, name|
-#       name.each do |name|
-#         pigeon_list[name] ||= {}
-#         pigeon_list[name][keys] ||= []
-#         pigeon_list[name][keys] << trait.to_s 
-#     end
-#   end
-# end
-# pigeon_list
+def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  data.each do |keys, value|
+    value.each do |trait, name|
+      name.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][keys] ||= []
+        pigeon_list[name][keys] << trait.to_s 
+    end
+  end
+end
+pigeon_list
   
-# end
+end
 
 
 
