b5c2aabc85cae16ff46d3a72f8ad866c17892582
Eugenia Kim <elk303@nyu.edu>
2019-07-29 20:35:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,7 +20,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |color, name|
       name.each do |name|
-        hash[name] = value 
+        hash[name] = color 
+        
 end
 
 
